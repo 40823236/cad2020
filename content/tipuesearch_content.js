badb70e3e479cd208d31467b58bf20547a62048f
40823236 <40823236@yahoo.com.tw>
2020-09-24 16:49:30 +0800

    test

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': '\n', 'tags': '', 'url': 'About.html'}, {'title': 'test', 'text': '', 'tags': '', 'url': 'test.html'}, {'title': 'test1', 'text': '', 'tags': '', 'url': 'test1.html'}]};
